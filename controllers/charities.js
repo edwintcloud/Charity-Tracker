c0f49290e113d92ed00b47fd872451882caf7658
Edwin Cloud <ecloud412@gmail.com>
2018-09-11 14:54:42 -0700

    CRUD Routes added

--- controllers/charities.js
+++ controllers/charities.js
@@ -1,9 +1,42 @@
 // IMPORT ROUTER
 const router = require('express').Router()
 
-// LANDING PAGE
+/* -----------------------
+  |  STATIC ROUTES BELOW  |
+   ----------------------- */
+    
+// HOME PAGE
 router.get('/', (req, res) => {
     res.render('home')
+})
+
+/* ---------------------
+  |  CRUD ROUTES BELOW  |
+   --------------------- */
+    
+// CREATE
+router.post('/users/:userId/charities/new', (req, res) => {
+    
+})
+
+// READ - ALL
+router.get('/users/:userId/charities', (req, res) => {
+    
+})
+
+// READ - SINGLE
+router.get('/users/:userId/charities/:charityId', (req, res) => {
+    
+})
+
+// UPDATE
+router.put('/users/:userId/charities/:charityId', (req, res) => {
+    
+})
+
+// DELETE
+router.delete('/users/:userId/charities/:charityId', (req, res) => {
+    
 })
 
 // EXPORT ROUTES
--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,42 @@
 // IMPORT ROUTER
 const router = require('express').Router()
 
+/* -----------------------
+  |  STATIC ROUTES BELOW  |
+   ----------------------- */
+    
 // USERS PAGE
 router.get('/users', (req, res) => {
     res.send('Hello user')
+})
+
+/* ---------------------
+  |  CRUD ROUTES BELOW  |
+   --------------------- */
+    
+// CREATE
+router.post('/users/new', (req, res) => {
+    
+})
+
+// READ - ALL
+router.get('/users', (req, res) => {
+    
+})
+
+// READ - SINGLE
+router.get('/users/:userId', (req, res) => {
+    
+})
+
+// UPDATE
+router.put('/users/:userId', (req, res) => {
+    
+})
+
+// DELETE
+router.delete('/users/:userId', (req, res) => {
+    
 })
 
 // EXPORT ROUTES
